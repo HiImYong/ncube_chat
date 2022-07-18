b24be08e0499c690dbc8bd3f75136364299e872d
tlsrb0025 <tlsrb0025@gmail.com>
2022-07-18 09:45:24 +0900

    provide ready3

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -92,16 +92,16 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'chat',
-        'USER': 'ncube',
-        'PASSWORD': 'ncube123',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'chat',
+#         'USER': 'ncube',
+#         'PASSWORD': 'ncube123',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
