cce31c15b16c4a9c406fe008c0d692464cb840ec
tlsrb0025 <tlsrb0025@gmail.com>
2022-07-18 10:29:21 +0900

    provide ready5

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -82,31 +82,31 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)], # Creates Redis connection.
+            "hosts": [('172.17.0.1', 6379)], # Creates Redis connection.
             
         },
     },
 }
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'chat',
-        'USER': 'sbsst',
-        'PASSWORD': 'sbs123414',
-        'HOST': '172.17.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            # 추가, 만약에 이 부분 때문에 오류가 난다면 삭제할 것
-            # MySQL Strict Mode 켜기
-            'charset': 'utf8mb4',
-            # 이모지 등록 가능
-            'use_unicode': True,
-        }
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'chat',
+#         'USER': 'sbsst',
+#         'PASSWORD': 'sbs123414',
+#         'HOST': '172.17.0.1',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#             # 추가, 만약에 이 부분 때문에 오류가 난다면 삭제할 것
+#             # MySQL Strict Mode 켜기
+#             'charset': 'utf8mb4',
+#             # 이모지 등록 가능
+#             'use_unicode': True,
+#         }
+#     }
+# }
 
 
 # Password validation
