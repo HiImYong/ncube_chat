1b4a5b217b119e23f1d559dc739ce69d1181d08e
tlsrb0025 <tlsrb0025@gmail.com>
2022-07-18 11:35:29 +0900

    provide ready6

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -89,24 +89,24 @@ CHANNEL_LAYERS = {
 }
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'chat',
-#         'USER': 'sbsst',
-#         'PASSWORD': 'sbs123414',
-#         'HOST': '172.17.0.1',
-#         'PORT': '3306',
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-#             # 추가, 만약에 이 부분 때문에 오류가 난다면 삭제할 것
-#             # MySQL Strict Mode 켜기
-#             'charset': 'utf8mb4',
-#             # 이모지 등록 가능
-#             'use_unicode': True,
-#         }
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'chat',
+        'USER': 'sbsst',
+        'PASSWORD': 'sbs123414',
+        'HOST': '172.17.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            # 추가, 만약에 이 부분 때문에 오류가 난다면 삭제할 것
+            # MySQL Strict Mode 켜기
+            'charset': 'utf8mb4',
+            # 이모지 등록 가능
+            'use_unicode': True,
+        }
+    }
+}
 
 
 # Password validation
@@ -183,3 +183,5 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
+
+CSRF_TRUSTED_ORIGINS = ['https://chat.uyong.site']
